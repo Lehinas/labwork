918552e2e11e94dacd1cd68e44458741272312f2
Lehinas <lehinas201283@mail.ru>
2024-12-20 20:20:27 +0500

    fix to deploy

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AuthService from "../services/AuthService"
 
-export const API_URL = "http://localhost:5000"
+export const API_URL = "https://labwork-zndl.onrender.com"
 
 export const api = axios.create({
     withCredentials: true,
